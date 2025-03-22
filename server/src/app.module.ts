d14ff50a518d0b2bf7bb6f66490a1e762fec9b26
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2025-03-21 21:54:09 -0300

    feat: health check

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { HealthController } from './health/health.controller';
+import { TerminusModule } from '@nestjs/terminus';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
+  imports: [
+    UsersModule,
+    TerminusModule,
+    HttpModule,
+  ],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
