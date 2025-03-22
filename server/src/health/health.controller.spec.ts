454f130214a6380fa0878449a4aa41ef17dbac14
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2025-03-21 22:25:24 -0300

    fix tests

--- server/src/health/health.controller.spec.ts
+++ server/src/health/health.controller.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { HealthCheckService, HttpHealthIndicator, HealthCheckResult } from '@nestjs/terminus';
 import { HealthController } from './health.controller';
 
 describe('HealthController', () => {
   let controller: HealthController;
+  let healthCheckService: HealthCheckService;
+  let httpHealthIndicator: HttpHealthIndicator;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HealthController],
+      providers: [
+        {
+          provide: HealthCheckService,
+          useValue: { check: jest.fn() },
+        },
+        {
+          provide: HttpHealthIndicator,
+          useValue: { pingCheck: jest.fn() },
+        },
+      ],
     }).compile();
 
     controller = module.get<HealthController>(HealthController);
+    healthCheckService = module.get<HealthCheckService>(HealthCheckService);
+    httpHealthIndicator = module.get<HttpHealthIndicator>(HttpHealthIndicator);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
 });
